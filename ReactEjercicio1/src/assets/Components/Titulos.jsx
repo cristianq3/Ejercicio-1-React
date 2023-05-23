dae56b218388dbd5a73618237880e08adb00f15c
cristianq3 <cristianq3@gmail.com>
2023-05-23 06:40:48 -0300

    Finalizado Ejercicio 1

---
+++ ReactEjercicio1/src/assets/Components/Titulos.jsx
@@ -0,0 +1,11 @@
+const Titulos = ({nuevoTitulo}) => {
+    // console.log(props.nuevoTitulo)
+
+  return (
+    <section>
+      <h1>Primer App react</h1> 
+    </section>
+  );
+};
+
+export default Titulos;
